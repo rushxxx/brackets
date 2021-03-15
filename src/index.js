0c8dce0ed90539cd14667dab8f15d1eaa18800dc
Rushxxx <rush.yu@gmail.com>
2021-03-15 16:04:17 +0500

    fix: bug

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('')
+  let res = true
+  while (res) {
+    res = false
+    for (let i = 0; i < arr.length - 1; i++) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+        if (arr[i] === bracketsConfig[j][0] && arr[i + 1] === bracketsConfig[j][1]) {
+          arr.splice(i, 2)
+          res = true
+        }
+      }
+    }
+  }
+  return arr.length === 0 ? true : false  
 }
